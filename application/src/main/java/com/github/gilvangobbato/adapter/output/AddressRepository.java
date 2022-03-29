ccac3780deb4998226823e42851e218f8509c05b
Gilvan André Gobbato <gilvangobbato@gmail.com>
2022-03-28 23:47:32 -0300

    Insert an address

--- application/src/main/java/com/github/gilvangobbato/adapter/output/AddressRepository.java
+++ application/src/main/java/com/github/gilvangobbato/adapter/output/AddressRepository.java
@@ -1,11 +1,9 @@
 package com.github.gilvangobbato.adapter.output;
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsync;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
 import com.amazonaws.services.dynamodbv2.document.Table;
-import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.github.gilvangobbato.domain.Address;
 import com.github.gilvangobbato.port.output.AddressPort;
@@ -35,23 +33,23 @@ public class AddressRepository implements AddressPort {
     @Override
     public Long insert(Address address) {
         Table table = dynamoDB.getTable(ADDRESS);
-        final Map<String, AttributeValue> infoMap = new HashMap<>();
+        final Map<String, Object> infoMap = new HashMap<>();
 
-        infoMap.put("city", new AttributeValue(address.getCity()));
-        infoMap.put("complement", new AttributeValue(address.getComplement()));
-        infoMap.put("country", new AttributeValue(address.getCountry()));
-        infoMap.put("ddd", new AttributeValue(address.getDdd()));
-        infoMap.put("district", new AttributeValue(address.getDistrict()));
-        infoMap.put("gia", new AttributeValue(address.getGia()));
-        infoMap.put("ibge_city", new AttributeValue(address.getIbgeCity()));
-        infoMap.put("place", new AttributeValue(address.getPlace()));
-        infoMap.put("ibge_country", new AttributeValue(address.getIbgeCountry()));
-        infoMap.put("siafi", new AttributeValue(address.getSiafi()));
-        infoMap.put("uf", new AttributeValue(address.getUf()));
-        infoMap.put("insert_date", new AttributeValue(LocalDateTime.now().toString()));
+        infoMap.put("city", address.getCity());
+        infoMap.put("complement", address.getComplement());
+        infoMap.put("country", address.getCountry());
+        infoMap.put("ddd", address.getDdd());
+        infoMap.put("district", address.getDistrict());
+        infoMap.put("gia", address.getGia());
+        infoMap.put("ibge_city", address.getIbgeCity());
+        infoMap.put("place", address.getPlace());
+        infoMap.put("ibge_country", address.getIbgeCountry());
+        infoMap.put("siafi", address.getSiafi());
+        infoMap.put("uf", address.getUf());
+        infoMap.put("insert_date", LocalDateTime.now().toString());
 
         try {
-            PutItemOutcome outcome = table.putItem(new Item().withPrimaryKey("cep", new AttributeValue(address.getCep())).withMap("Address", infoMap));
+            PutItemOutcome outcome = table.putItem(Item.fromMap(infoMap).withPrimaryKey("cep", address.getCep()));
             System.out.println(outcome.getPutItemResult().toString());
         } catch (Exception ex) {
             ex.printStackTrace();
